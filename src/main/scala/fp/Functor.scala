36397fd19c5803b86028bc1a64eaa64ea85d3e62
Lander Lopez <lander.lopez@ihsmarkit.com>
2018-05-08 17:59:41 +0100

    Remark

---
+++ src/main/scala/fp/Functor.scala
@@ -0,0 +1,16 @@
+package fp
+
+
+trait Reify[F[_], A]
+case class Pure[F[_], A](fa: F[A]) extends Reify[F, A]
+case class Map[F[_], A, B](a: Reify[F, A], f: A => B) extends Reify[F, B]
+case class FlatMap[F[_], A, B](a: Reify[F, A], f: A => F[B]) extends Reify[F, B]
+
+
+
+object ID {
+  type ID[A] = A
+
+  def interpret[A](f: Reify[F.  ])
+
+}
--- src/test/scala/stacc/logic/OrthogonalitySpec.scala
+++ src/test/scala/stacc/logic/OrthogonalitySpec.scala
@@ -11,8 +11,10 @@ class OrthogonalitySpec extends FreeSpec {
     val A = ConcreteVarPSet('a := empty)
     val Az = ConcreteVarPSet('z := empty, 'a := empty)
     val B = ConcreteVarPSet('b := empty)
+    val XA = ConcreteVarPSet('a := B)
 
-    "orthogonal" in assert { relation(A, B) === Orthogonal(A, B) }
+    "orthogonal" in assert { relation(A, B) === Orthogonal(A, B)
     "Subsumed" in assert { relation(A, Az) === Subsumption(Az, A) }
+    "incompatible" in assert { relation(A, XA) === Incompatible(A, XA) }
   }
 }
